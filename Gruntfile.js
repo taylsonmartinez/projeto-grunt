ca5f79334b7cad4181b85b34da3d2a78147ac412
Taylson Molina Martinez <taylson.martinez@gmail.com>
2015-07-13 14:38:50 -0300

    Minificação e concatenação

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,12 +12,30 @@ module.exports = function(grunt) {
             dist: {
                 src: 'dist'
             }
+        },
+        useminPrepare:{
+            html: 'dist/**/*.html'
+        },
+        usemin: {
+            html: 'dist/**/*.html'
         }
+
     });
-
+    //registrando task para minificacao
     grunt.registerTask('dist', ['clean', 'copy']);
-    grunt.registerTask('default', ['dist']);
 
+    grunt.registerTask('minifica', ['useminPrepare',
+        'concat', 'uglify', 'cssmin', 'usemin']);
+
+    // registrando tasks
+    grunt.registerTask('default', ['dist', 'minifica']);
+
+
+    // carregando tasks
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-usemin');
 };
