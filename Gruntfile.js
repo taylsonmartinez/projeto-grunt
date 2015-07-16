2557a6ded692a30d8f966be1ff9197c6d0607471
Taylson Molina Martinez <taylson.martinez@gmail.com>
2015-07-16 16:46:52 -0300

    pré-processadores

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,7 +41,46 @@ module.exports = function(grunt) {
             minificados: {
                 src: ['dist/js/**/*.min.js', 'dist/css/**/*.min.css']
             }
+        },
+        coffee: {
+            compilar: {
+                expand: true,
+                cwd: 'public/coffee',
+                src: ['**/*.coffee'],
+                dest: 'public/js',
+                ext: '.js'
+
+            }
+        } ,
+
+        less: {
+            compilar: {
+                expand: true,
+                cwd: 'public/less',
+                src: ['**/*.less'],
+                dest: 'public/css',
+                ext: '.css'
+            }
+        },
+        watch: {
+
+            coffee: {
+                options: {
+                    event: ['added', 'changed']
+                },
+                files: 'public/coffee/**/*.coffee',
+                tasks: 'coffee:compilar'
+            },
+
+            less: {
+                options: {
+                    event: ['added', 'changed']
+                },
+                files: 'public/less/**/*.less',
+                tasks: 'less:compilar'
+            }
         }
+
 
     });
     //registrando task para minificacao
@@ -63,4 +102,8 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-usemin');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-rev');
+    grunt.loadNpmTasks('grunt-contrib-coffee');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
 };
---
+++ public/js/teste.js
@@ -0,0 +1,6 @@
+(function() {
+  var nome;
+
+  nome = "Taylson Martinez";
+
+}).call(this);
