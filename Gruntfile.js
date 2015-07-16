035f58b644dfbef14700d3dfcb61d4df889337d0
Taylson Molina Martinez <taylson.martinez@gmail.com>
2015-07-16 16:14:43 -0300

    versionamento

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,6 +26,21 @@ module.exports = function(grunt) {
                 src: '**/*.{png,jpg,gif}',
                 dest: 'dist/img'
             }
+        },
+        rev: {
+            options: {
+                encoding: 'utf8',
+                algorithm: 'md5',
+                length: 8
+            },
+
+            imagens: {
+                src: ['dist/img/**/*.{png,jpg,gif}']
+            },
+
+            minificados: {
+                src: ['dist/js/**/*.min.js', 'dist/css/**/*.min.css']
+            }
         }
 
     });
@@ -33,7 +48,7 @@ module.exports = function(grunt) {
     grunt.registerTask('dist', ['clean', 'copy']);
 
     grunt.registerTask('minifica', ['useminPrepare',
-        'concat', 'uglify', 'cssmin', 'usemin', 'imagemin']);
+        'concat', 'uglify', 'cssmin','rev', 'usemin', 'imagemin']);
 
     // registrando tasks
     grunt.registerTask('default', ['dist', 'minifica']);
@@ -47,4 +62,5 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-usemin');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-rev');
 };
